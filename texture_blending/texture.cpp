74fdfa1d60a7b4572b845a034da66919fedb7c98
maximhrunenko <maxhrunenko@gmail.com>
2024-03-08 23:45:28 +0200

    load texture

---
+++ texture_blending/texture.cpp
@@ -0,0 +1,66 @@
+#include <SDL2/SDL_error.h>
+#include <iostream>
+#include <SDL2/SDL.h>
+
+constexpr int WIDTH {800};
+constexpr int HEIGHT {600};
+
+
+int main() {
+
+    SDL_Window *win {};
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL could not be initializec" << SDL_GetError() << '\n';
+    } else {
+        std::cout << "SDL video enabled\n";
+    }
+
+    win = SDL_CreateWindow("Texture win", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer *rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Surface *sur = SDL_LoadBMP("./ft.bmp");
+
+    SDL_SetColorKey(sur, SDL_TRUE, SDL_MapRGB(sur->format, 0xFF, 0, 0xFF));
+    SDL_Texture *tex = SDL_CreateTextureFromSurface(rend, sur);
+    SDL_FreeSurface(sur);
+
+    SDL_Rect rect {.x = 50, .y = 100, .w = 200, .h = 200};
+
+    bool run =true;
+
+    SDL_Event e;
+    while(run) {
+
+        // while(SDL_HasEvent(!SDL_QUIT)) {
+        //     if (e.type == SDL_QUIT) {
+        //         run = false;
+        //     }
+        // }
+
+        while(SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                run = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(rend, 0, 0, 0xFF, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(rend);
+
+        SDL_SetRenderDrawColor(rend, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawLine(rend, 5, 5, 200, 200);
+
+        SDL_RenderCopy(rend, tex, NULL, &rect);
+
+
+        SDL_RenderPresent(rend);
+
+    }
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
+
+    return 0;
+}
