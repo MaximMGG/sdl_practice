f384ff61a46e16bcf100be09f097284813755f81
maximhrunenko <maxhrunenko@gmail.com>
2024-03-09 22:25:54 +0200

    texture blending and collision of objects

--- texture_blending/texture.cpp
+++ texture_blending/texture.cpp
@@ -5,6 +5,11 @@
 constexpr int WIDTH {800};
 constexpr int HEIGHT {600};
 
+bool rect_is_collision(SDL_Rect& x, SDL_Rect &y) {
+    return (x.x + x.w < y.x || x.y > y.y + y.h || x.x > y.x + y.w ||  x.y + x.h < y.y);
+}
+
+
 
 int main() {
 
@@ -24,10 +29,11 @@ int main() {
 
     SDL_SetColorKey(sur, SDL_TRUE, SDL_MapRGB(sur->format, 0xFF, 0, 0xFF));
     SDL_Texture *tex = SDL_CreateTextureFromSurface(rend, sur);
+
     SDL_FreeSurface(sur);
 
-    SDL_Rect rect {.x = 50, .y = 100, .w = 200, .h = 200};
-    SDL_Rect rect2 {.x = 250, .y = 300, .w = 200, .h = 200};
+    SDL_Rect rect {.x = WIDTH / 2 - 100 / 2, .y = HEIGHT / 2 - 100 / 2, .w = 100, .h = 100};
+    SDL_Rect rect2 {.x = 250, .y = 300, .w = 100, .h = 100};
 
     bool run =true;
 
@@ -50,13 +56,28 @@ int main() {
                 rect2.x = e.motion.x;
                 rect2.y = e.motion.y;
             }
+            if (!rect_is_collision(rect, rect2)) {
+                SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_ADD);
+            } else {
+                SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_NONE);
+            }
+            // if (e.type == SDL_MOUSEBUTTONDOWN) {
+            //     if (e.button.button == SDL_BUTTON_LEFT) {
+            //         SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_ADD);
+            //     }
+            // }
+            // if (e.type == SDL_MOUSEBUTTONUP) {
+            //     if (e.button.button == SDL_BUTTON_LEFT) {
+            //         SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_NONE);
+            //     }
+            // }
         }
 
         SDL_SetRenderDrawColor(rend, 0, 0, 0xFF, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(rend);
 
-        SDL_SetRenderDrawColor(rend, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
-        SDL_RenderDrawLine(rend, 5, 5, 200, 200);
+        // SDL_SetRenderDrawColor(rend, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
+        // SDL_RenderDrawLine(rend, 5, 5, 200, 200);
 
         SDL_RenderCopy(rend, tex, NULL, &rect);
         SDL_RenderCopy(rend, tex, NULL, &rect2);
