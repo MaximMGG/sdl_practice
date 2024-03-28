161df11da534c5c67ad63292c7c95d4c08ac5bf5
maximhrunenko <maxhrunenko@gmail.com>
2024-03-28 21:42:12 +0200

    write whole func

--- water_texture/water.c
+++ water_texture/water.c
@@ -3,8 +3,8 @@
 
 #define serror(msg) fprintf(stderr, "%s - %s\n", msg, SDL_GetError());
 
-#define WIDTH 800
-#define HEIGHT 600
+#define WIDTH 640
+#define HEIGHT 480
 
 
 int main() {
@@ -46,10 +46,55 @@ int main() {
                     if (e.type == SDL_QUIT) {
                         run = 0;
                     }
+                    if (e.type == SDL_MOUSEBUTTONDOWN) {
+                        if (e.button.button == SDL_BUTTON_LEFT) {
+                            SDL_SetTextureBlendMode(tex2, SDL_BLENDMODE_ADD);
+                        } else if (e.button.button == SDL_BUTTON_MIDDLE) {
+                            SDL_SetTextureBlendMode(tex2, SDL_BLENDMODE_BLEND);
+                        } else if (e.button.button == SDL_BUTTON_RIGHT) {
+                            SDL_SetTextureBlendMode(tex2, SDL_BLENDMODE_MOD);
+                        }
+                    }
                 }
+                SDL_SetRenderDrawColor(rend, 0, 0, 0xFF, SDL_ALPHA_OPAQUE);
+                SDL_RenderClear(rend);
+
+                int w, h;
+                SDL_QueryTexture(tex1, NULL, NULL, &w, &h);
+                SDL_Delay(20);
+
+                rect1.x++;
+                if (rect1.x > 639) {
+                    rect1.x = -639;
+                }
+                rect2.x++;
+                if (rect2.x > 639) {
+                    rect2.x = -639;
+                }
+                rect3.y++;
+                if (rect3.y > 479) {
+                    rect3.y = -480;
+                }
+                rect4.y++;
+                if (rect4.y > 479) {
+                    rect4.y = -480;
+                }
+
+                SDL_SetTextureBlendMode(tex1, SDL_BLENDMODE_NONE);
+                SDL_RenderCopy(rend, tex1, NULL, &rect1);
+                SDL_RenderCopy(rend, tex1, NULL, &rect2);
+
+                SDL_RenderCopy(rend, tex2, NULL, &rect3);
+                SDL_RenderCopy(rend, tex2, NULL, &rect4);
+
+
+                SDL_RenderPresent(rend);
             }
 
         }
+
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
     }
 
 
