88917358c0dce309b658fbf27b880eac142af1eb
Maxim Hrunenko <maxhrunenko@gmail.com>
2024-04-18 19:11:47 +0300

    mouse position done

--- send/senc.c
+++ send/senc.c
@@ -20,74 +20,66 @@ struct point *create_point(int x, int y) {
     return new;
 }
 
-void do_gravity(List *send) {
-    for(int i = 0; i < send->len; i++) {
-        struct point *temp = (struct point *)list_get(send, i);
-        if (temp->x == HEIGHT - 1) {
-            continue;
-        } else {
-            temp->y++;
-            table[temp->x - 1][temp->y] = 0;
-            table[temp->x][temp->y] = 1;
+void do_gravity(List *l) {
+    for(int i = 0; i < l->len; i++) {
+        struct point *p = list_get(l, i);
+        if (p->y) {
+            p->y++;
         }
     }
 }
 
-void draw_send(SDL_Window *win, SDL_Renderer *rend, List *send) {
-
-    SDL_SetRenderDrawColor(rend, 0xFF, 0xFF, 0xFF, 0xFF);
-    for(int i = 0; i < send->len; i++) {
-        struct point *temp = (struct point *) list_get(send, i);
-        SDL_RenderDrawPoint(rend, temp->x, temp->y);
-    }
-    SDL_RenderPresent(rend);
-}
-
-
-void free_list(List *l) {
+void draw_send(SDL_Renderer *rend, List *l) {
     for(int i = 0; i < l->len; i++) {
-        struct point *t = (struct point *)list_get(l, i);
-        free(t);
+        struct point *p = list_get(l, i);
+        SDL_RenderDrawPoint(rend, p->x, p->y);
     }
-
-    list_free_all(l);
 }
 
 
 int main() {
-    List *l = list_create(0, l_struct);
 
     SDL_Window *win;
     SDL_Renderer *rend;
+    List *l = list_create(0, l_struct);
+
     SDL_Init(SDL_INIT_VIDEO);
 
-    win = SDL_CreateWindow("Send", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-    rend = SDL_CreateRenderer(win, 0, SDL_RENDERER_ACCELERATED);
+    win = SDL_CreateWindow("send", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
     SDL_Event e;
     int run = 1;
-    int mx = 0;
-    int my = 0;
-
+    int mx, my;
 
     while(run) {
-        while (SDL_PollEvent(&e)) {
+        while(SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
                 run = 0;
             }
-
             if (e.type == SDL_MOUSEBUTTONDOWN) {
                 SDL_GetMouseState(&mx, &my);
+                fprintf(stderr, "mouse x = %d, y = %d\n", mx, my);
+
                 if (mx <= WIDTH && my <= HEIGHT) {
-                    list_add(l, create_point(mx, my));
+                    struct point *p = create_point(mx, my);
+                    list_add(l, p);
                 }
             }
         }
-        do_gravity(l);
-        draw_send(win, rend, l);
+
         SDL_SetRenderDrawColor(rend, 0x00, 0x00, 0x00, 0xFF);
         SDL_RenderClear(rend);
+
+        SDL_SetRenderDrawColor(rend, 0xFF, 0xFF, 0xFF, 0xFF);
+        do_gravity(l);
+        draw_send(rend, l);
+
+        SDL_RenderPresent(rend);
+
     }
+
+    list_free_all(l);
 
     SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
